d2f14938d0f1eb4eeafb3b30449098c88638208c
aaamundial <henrypaul2000@hotmail.com>
2025-06-10 13:58:05 -0500

    factura

--- src/main/java/com/aaamundial/xades/XadesSignerService.java
+++ src/main/java/com/aaamundial/xades/XadesSignerService.java
@@ -5,10 +5,14 @@ import xades4j.providers.impl.DirectKeyingDataProvider;
 import xades4j.utils.*;
 
 import org.w3c.dom.Document;
-
+import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.security.*;
 import java.security.cert.X509Certificate;
+import java.io.ByteArrayOutputStream;
 
 public class XadesSignerService {
 
@@ -18,11 +22,11 @@ public class XadesSignerService {
         KeyStore ks = KeyStore.getInstance("PKCS12");
         ks.load(new java.io.ByteArrayInputStream(p12Bytes), pwd.toCharArray());
         String alias = ks.aliases().nextElement();
-        PrivateKey pk = (PrivateKey) ks.getKey(alias, pwd.toCharArray());
         X509Certificate cert = (X509Certificate) ks.getCertificate(alias);
+        PrivateKey pk       = (PrivateKey) ks.getKey(alias, pwd.toCharArray());
 
         // 2) Perfil XAdES-BES SHA-256
-        DirectKeyingDataProvider kdp = new DirectKeyingDataProvider(pk, cert);
+        DirectKeyingDataProvider kdp = new DirectKeyingDataProvider(cert, pk);
         XadesSigner signer = new XadesBesSigningProfile(kdp)
                 .withSignatureMethod(SignatureMethod.RSA_SHA256)
                 .withDigestMethod(DigestMethod.SHA256)
@@ -34,9 +38,24 @@ public class XadesSignerService {
                 .newDocumentBuilder()
                 .parse(new java.io.ByteArrayInputStream(xmlBytes));
 
-        // 4) Enveloped sobre el nodo raíz
-        signer.sign(doc, new Enveloped(doc.getDocumentElement()));
+        // Objeto firmado → “enveloped” sobre nodo raíz
+        DataObjectDesc obj = new Enveloped(documentRoot(doc));
+        signer.sign(new SignedDataObjects(obj), doc.getDocumentElement());
 
-        return DOMHelper.domToByteArray(doc, true);
+        return toBytes(doc);
+
+    }
+
+    /** nodo raíz */
+    private static Element documentRoot(Document doc) {
+        return doc.getDocumentElement();
+    }
+
+    /** Convierte el DOM a bytes UTF-8 */
+    private static byte[] toBytes(Document doc) throws TransformerException {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        Transformer tf = TransformerFactory.newInstance().newTransformer();
+        tf.transform(new DOMSource(doc), new StreamResult(out));
+        return out.toByteArray();
     }
 }

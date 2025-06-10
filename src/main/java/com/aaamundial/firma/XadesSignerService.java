63e88dd8ea864256ca13dc9f92c7377752322594
aaamundial <henrypaul2000@hotmail.com>
2025-06-10 14:33:53 -0500

    factura

--- src/main/java/com/aaamundial/firma/XadesSignerService.java
+++ src/main/java/com/aaamundial/firma/XadesSignerService.java
@@ -5,7 +5,6 @@ import xades4j.production.*;
 import xades4j.providers.impl.DirectKeyingDataProvider;
 import xades4j.properties.DataObjectFormatProperty;
 import xades4j.algorithms.EnvelopedSignatureTransform;
-// ¡IMPORTAMOS LA CLASE QUE VAMOS A USAR AHORA!
 import xades4j.properties.DataObjectDesc;
 
 import org.w3c.dom.Document;
@@ -18,17 +17,46 @@ import javax.xml.transform.stream.StreamResult;
 import java.io.ByteArrayOutputStream;
 import java.security.*;
 import java.security.cert.X509Certificate;
+import java.util.Enumeration; // Necesaria para iterar sobre los alias
 
 public class XadesSignerService {
 
     public byte[] sign(byte[] xmlBytes, byte[] p12Bytes, String pwd) throws Exception {
 
-        /* ---------- 1) Certificado y clave del PKCS#12 ---------- */
+        /* ---------- 1) Certificado y clave del PKCS#12 (CON BÚSQUEDA DE ALIAS) ---------- */
         KeyStore ks = KeyStore.getInstance("PKCS12");
         ks.load(new java.io.ByteArrayInputStream(p12Bytes), pwd.toCharArray());
-        String alias = ks.aliases().nextElement();
-        X509Certificate cert = (X509Certificate) ks.getCertificate(alias);
-        PrivateKey      key  = (PrivateKey) ks.getKey(alias, pwd.toCharArray());
+        
+        String signingAlias = null;
+        Enumeration<String> aliases = ks.aliases();
+
+        while (aliases.hasMoreElements()) {
+            String currentAlias = aliases.nextElement();
+            // Buscamos el alias que es para firmar ("signing key")
+            if (currentAlias.toLowerCase().contains("signing key")) {
+                signingAlias = currentAlias;
+                break;
+            }
+        }
+
+        if (signingAlias == null) {
+            // Si no lo encontramos, como plan B, tomamos el primero que tenga clave privada
+            aliases = ks.aliases();
+            while (aliases.hasMoreElements()) {
+                String currentAlias = aliases.nextElement();
+                if (ks.isKeyEntry(currentAlias)) {
+                    signingAlias = currentAlias;
+                    break;
+                }
+            }
+        }
+        
+        if (signingAlias == null) {
+            throw new KeyStoreException("No se encontró un alias válido para firmar en el archivo P12.");
+        }
+
+        X509Certificate cert = (X509Certificate) ks.getCertificate(signingAlias);
+        PrivateKey key = (PrivateKey) ks.getKey(signingAlias, pwd.toCharArray());
 
         /* ---------- 2) Perfil XAdES ---------- */
         DirectKeyingDataProvider kdp = new DirectKeyingDataProvider(cert, key);
@@ -41,12 +69,10 @@ public class XadesSignerService {
                 .parse(new java.io.ByteArrayInputStream(xmlBytes));
 
         /* ---------- 4) Objeto firmado (enveloped) ---------- */
-        // AQUÍ ESTÁ LA CORRECCIÓN: La variable 'obj' ahora es del tipo correcto 'DataObjectDesc'
         DataObjectDesc obj = new DataObjectReference("")
                 .withTransform(new EnvelopedSignatureTransform())
                 .withDataObjectFormat(new DataObjectFormatProperty("text/xml"));
         
-        // El constructor de SignedDataObjects puede aceptar un DataObjectDesc
         SignedDataObjects signedDataObjects = new SignedDataObjects(obj);
         
         signer.sign(signedDataObjects, doc.getDocumentElement());
